5b4d1bdc87ef80cec0349da9428744b0c8032308
Fernando Mercadal <fermercadal@outlook.com>
2020-04-27 19:54:13 -0300

    Updates

---
+++ precache-manifest.2f771efaa4427baaf8fb20dfc285d5fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6af202e2bcd9301e12a7f45f7fe1038",
+    "url": "/markdown-preview/index.html"
+  },
+  {
+    "revision": "6a242ac2bf04b901d94c",
+    "url": "/markdown-preview/static/css/main.c2704612.chunk.css"
+  },
+  {
+    "revision": "afd48cd1be0a5e1d45c2",
+    "url": "/markdown-preview/static/js/2.bf62df83.chunk.js"
+  },
+  {
+    "revision": "6a242ac2bf04b901d94c",
+    "url": "/markdown-preview/static/js/main.2368e9fa.chunk.js"
+  },
+  {
+    "revision": "f6bf229f15cdf2c9779f",
+    "url": "/markdown-preview/static/js/runtime-main.af69ec20.js"
+  }
+]);
